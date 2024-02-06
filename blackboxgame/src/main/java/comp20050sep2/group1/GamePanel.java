6aa0c25630d07f61f2cab6acbb3fdd76be4bb543
ma-ng-o <banerjee.tamoghno@ucdconnect.ie>
2024-02-06 17:22:28 +0000

    Added paintComponent() to be called for updates

--- blackboxgame/src/main/java/comp20050sep2/group1/GamePanel.java
+++ blackboxgame/src/main/java/comp20050sep2/group1/GamePanel.java
@@ -1,11 +1,18 @@
 package comp20050sep2.group1;
 
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
     
     public GamePanel(){
-        
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        //render graphics
     }
 
 }
