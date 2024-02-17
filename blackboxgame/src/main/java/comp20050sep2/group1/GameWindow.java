510da756f8790a45ca2d3a5163d7078eb3d9bb40
ma-ng-o <banerjee.tamoghno@ucdconnect.ie>
2024-02-17 06:29:27 -0500

    Added card layout for multiple panels

--- blackboxgame/src/main/java/comp20050sep2/group1/GameWindow.java
+++ blackboxgame/src/main/java/comp20050sep2/group1/GameWindow.java
@@ -1,16 +1,32 @@
 package comp20050sep2.group1;
 
+import java.awt.CardLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameWindow extends JFrame{
     
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+    
     GameWindow(GamePanel gamePanel){
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        add(gamePanel);
+
+        cardPanel = new JPanel();
+        cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+
+        cardPanel.add(gamePanel, "Game Panel");
+
+        add(cardPanel);
+
         setLocationRelativeTo(null);
         setResizable(false);
         pack();
         setVisible(true);
+
     }
 
 }
