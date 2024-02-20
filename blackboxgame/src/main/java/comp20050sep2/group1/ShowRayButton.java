529368fd84e2744a27ae3322739c9ebd34f086e3
Juutar <108992842+Juutar@users.noreply.github.com>
2024-02-20 10:22:47 +0000

    AbstractShowButton superclass

--- blackboxgame/src/main/java/comp20050sep2/group1/ShowRayButton.java
+++ blackboxgame/src/main/java/comp20050sep2/group1/ShowRayButton.java
@@ -8,23 +8,15 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 
-public class ShowRayButton extends JButton{
-
-    int width = 150;
-    int height = 40;
-
+public class ShowRayButton extends AbstractShowButton {
+    
     public ShowRayButton(Vector2D pos){
-        setLocation((int) pos.x - width/2, (int) pos.y - height/2);
-        setSize(150, 40);
-        setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
-        setFocusable(false);
-        setVisible(true);
-
-        setText("Set Rays");
-        setBackground(new Color(0.0F, 0.F, 0.F, 0.5F));
-        setForeground(Color.white);
-        setBorder(new LineBorder(Color.white, 1));
-
+        super(pos);
     }
 
+    @Override
+    public void setCustomTexts() {
+        showText = "Set Rays";
+        hideText = "Set Atoms";
+    }
 }
