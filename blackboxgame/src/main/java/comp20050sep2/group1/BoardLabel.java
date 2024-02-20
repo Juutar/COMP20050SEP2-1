abf1f395457ca98d8c37a5a229c4089f23155e35
ma-ng-o <banerjee.tamoghno@ucdconnect.ie>
2024-02-20 18:58:56 +0000

    Changed code for BoardLabel

--- blackboxgame/src/main/java/comp20050sep2/group1/BoardLabel.java
+++ blackboxgame/src/main/java/comp20050sep2/group1/BoardLabel.java
@@ -9,47 +9,20 @@ import javax.swing.JLabel;
 public class BoardLabel extends JLabel{
     
     private String text;
-    private Hexagon h;
 
-    public Hexagon getH() {
-        return h;
-    }
+    double x;
+    double y;
 
-    BoardLabel(String text, Hexagon h){
+    BoardLabel(String text){
 
         this.text = text;
-        this.h = h;
-
-        int index = Integer.parseInt(text) - 19;
-
-        if(index % 3 == 0){
-            this.h.pointableSides = 3;
-        }
-        else{
-            this.h.pointableSides = 2;
-        }
-
-        if(index <= 25){
-            //vector = 
-            //angle = 120
-            //if i % 3 == 0 --> angle +/-= 30
-        }
-        else if(index <= 27){
-            //
-        }
-        else if(index <= 34){
-            
-        }
-        else if(index <= 36){
-            
-        }
 
     }
 
     public void writeText(int xOffset, int yOffset){
         GamePanel.get().graphics.setColor(new Color(255, 150, 150));
         GamePanel.get().graphics.setFont(new Font("Arial", Font.TYPE1_FONT, 15));
-        GamePanel.get().graphics.drawString(text, (int)h.center().x + xOffset, (int)h.center().y + yOffset);
+        GamePanel.get().graphics.drawString(text, (int)x, (int)y);
     }
 
 }
