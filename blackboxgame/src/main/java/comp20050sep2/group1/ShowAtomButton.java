fc527f996985257b5de5b8747be5cf6d3ea1bfae
Vaxry <vaxry@vaxry.net>
2024-04-02 12:41:54 +0100

    ui: quit to menu on ended game

--- blackboxgame/src/main/java/comp20050sep2/group1/ShowAtomButton.java
+++ blackboxgame/src/main/java/comp20050sep2/group1/ShowAtomButton.java
@@ -2,7 +2,11 @@ package comp20050sep2.group1;
 
 import comp20050sep2.group1.utils.Vector2D;
 
+import javax.swing.*;
+
 public class ShowAtomButton extends AbstractShowButton {
+
+    private boolean ended = false;
 
     public ShowAtomButton(Vector2D pos) {
         super(pos, "End Game", "Game Ended");
@@ -10,6 +14,13 @@ public class ShowAtomButton extends AbstractShowButton {
 
     @Override
     public void performAction() {
+        if (ended) {
+            GamePanel.destroy();
+            MainMenuPanel.get().gameFrame = null;
+            SwingUtilities.getWindowAncestor(MainMenuPanel.get()).setVisible(true);
+            return;
+        }
+
         if (stateShow) {
             if (GamePanel.get().board.allGuessAtomsPlaced()) {
                 toggleState();
@@ -17,6 +28,8 @@ public class ShowAtomButton extends AbstractShowButton {
                 GamePanel.get().board.toggleRaysVisible();
                 GamePanel.get().board.toggleEvaluate();
                 GamePanel.get().board.toggleScore();
+                ended = true;
+                setButtonText("Quit to menu");
             } else {
                 GamePanel.get().outputBox.setText("Place 6 atoms");
             }
