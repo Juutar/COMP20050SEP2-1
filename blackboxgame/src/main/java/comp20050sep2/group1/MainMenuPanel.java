0998b40f609d98adfab09b49d6799d5c8d7b36a1
ma-ng-o <banerjee.tamoghno@ucdconnect.ie>
2024-04-07 20:25:51 +0100

    Fixed minor bug on MainMenuPanel

--- blackboxgame/src/main/java/comp20050sep2/group1/MainMenuPanel.java
+++ blackboxgame/src/main/java/comp20050sep2/group1/MainMenuPanel.java
@@ -5,7 +5,6 @@ import comp20050sep2.group1.utils.Vector2D;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Objects;
 
 public class MainMenuPanel extends JPanel implements MouseListener, WindowListener {
 
