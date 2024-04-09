1504b76ec3cc466486fab897339996ab816b7603
Juutar <108992842+Juutar@users.noreply.github.com>
2024-04-09 09:57:14 +0100

    added output box, rearranged button positions

--- blackboxgame/src/main/java/comp20050sep2/group1/MainMenuPanel.java
+++ blackboxgame/src/main/java/comp20050sep2/group1/MainMenuPanel.java
@@ -35,9 +35,10 @@ public class MainMenuPanel extends JPanel implements MouseListener, WindowListen
         this.addMouseListener(this);
         this.setLayout(null);
 
-        outputBox = new OutputBox(new Vector2D(this.getWidth()/2.0, this.getHeight()/3.0));
+        outputBox = new OutputBox(new Vector2D(screenWidth/4.0, screenHeight/5.0));
+        outputBox.setText("No score so far");
 
-        playButton = new PlayButton(new Vector2D(screenWidth/4.0, screenHeight/4.0));
+        playButton = new PlayButton(new Vector2D(screenWidth/4.0, screenHeight/3.0));
         playButton.addMouseListener(this);
         this.add(playButton);
 
@@ -61,9 +62,11 @@ public class MainMenuPanel extends JPanel implements MouseListener, WindowListen
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
-        g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 40));
         g.setColor(Color.WHITE);
-        g.drawString(text, this.getWidth()/2 - g.getFontMetrics().stringWidth(text)/2, this.getHeight()/4);
+        g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 40));
+        g.drawString(text, this.getWidth()/2 - g.getFontMetrics().stringWidth(text)/2, this.getHeight()/5);
+        g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20));
+        g.drawString("Best Score:", this.getWidth()/2 - g.getFontMetrics().stringWidth("Best Score:")/2, this.getHeight()/5 + 50);
 
         if (outputBox != null) {
             outputBox.drawOutputBox((Graphics2D) g);
